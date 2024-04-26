587193b2ce3e93f5368592733e22e1dc3c7b92f8
tmachine <tmachine@SYS22.PEC.IN>
2024-04-26 09:19:29 +0530

    Reference.txt

--- ScreenCapture.py
+++ ScreenCapture.py
@@ -3,10 +3,14 @@ import time
 import subprocess
 
 def is_browser_window_open():
-    
+    browser_processes = ['chrome.exe', 'firefox.exe', 'msedge.exe']
     process = subprocess.Popen(['tasklist'], stdout=subprocess.PIPE)
     output, _ = process.communicate()
-    return b'chrome.exe' in output  
+    for browser_process in browser_processes:
+        if browser_process.encode() in output:
+            return True
+    return False
+      
 
 def capture_screenshots_with_browser(interval=10, duration=60):
     end_time = time.time() + duration
